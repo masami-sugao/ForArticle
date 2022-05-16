7d6534d36be9587d3c5a36c9156d91e3ba7dfc3c
Masami Sugao <15377854+masami-sugao@users.noreply.github.com>
2022-05-16 21:36:41 +0900

    :alembic: Non-Uniform Scaling確認シーン追加

---
+++ Assets/Scripts/NonuniformScaling/ScaleTeller.cs
@@ -0,0 +1,13 @@
+﻿#nullable enable
+using UnityEngine;
+
+namespace NonuniformScaling
+{
+    public class ScaleTeller : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log($"[{this.name}] LossyScale: {this.transform.lossyScale} LocalScale: {this.transform.localScale}");
+        }
+    }
+}
