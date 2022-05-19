4565891a1803b5f296e7462de89d44cfa62d8ac2
Masami Sugao <15377854+masami-sugao@users.noreply.github.com>
2022-05-19 18:03:36 +0900

    :sparkles: 親のScaleの影響を排除する実験に行列利用版追加

--- Assets/Scripts/KeepingScale/ScaleKeeper.cs
+++ Assets/Scripts/KeepingScale/ScaleKeeper.cs
@@ -7,18 +7,30 @@ namespace KeepingScale
     {
         private void Start()
         {
+            Debug.Log($"localScale/lossyScale {this.transform.localScale.x / this.transform.lossyScale.x}");
+            Debug.Log($"localScale/ParentLossyScale {this.transform.localScale.x / this.transform.parent.lossyScale.x}");
+
+            this.DoByMatrix();
+            // this.DoByTransformMethods();
+        }
+        
+        private void DoByTransformMethods()
+        {
             // localScaleを表すベクトルをワールドの向きに変換
             var worldDirectedLocalScale
                 = this.transform.parent.TransformDirection(this.transform.localScale);
             Debug.Log($"worldDirectedLocalScal: {worldDirectedLocalScale}");
-            Debug.Log($"cos75: {Mathf.Cos(75 / 360.0f * 2 * Mathf.PI) * Mathf.Sqrt(2) * 0.75} sin75: {Mathf.Sin(75 / 360.0f * 2 * Mathf.PI) * Mathf.Sqrt(2) * 0.75}");
 
             // ワールド上で上記ベクトルを示す場合のローカル上での表現方法を算出
             // これがlocalScale設定値の大きさをワールド上で実現するために必要なlocalScaleとなる
             var scaleUnaffectedByParents = this.transform.parent.InverseTransformVector(worldDirectedLocalScale);
             this.transform.localScale = scaleUnaffectedByParents;
-            Debug.Log($"localScale/lossyScale {this.transform.localScale.x / this.transform.lossyScale.x}");
-            Debug.Log($"localScale/ParentLossyScale {this.transform.localScale.x / this.transform.parent.lossyScale.x}");
+        }
+        
+        private void DoByMatrix()
+        {
+            var parentScale = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, this.transform.parent.lossyScale);
+            this.transform.localScale = parentScale.inverse * this.transform.lossyScale;
         }
     }
 }
